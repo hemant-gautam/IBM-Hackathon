cdf019027446a537ca662cfe8d6aa054656d5f22
hemant gautam <vishhemantgautam@gmail.com>
2018-03-11 20:15:20 +0530

    Update views.py

--- mysite/pollapp/music/views.py
+++ mysite/pollapp/music/views.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
+import os
 from os.path import join, dirname
-
+import sys
 import requests
-from django.shortcuts import render
+import subprocess
+
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render, redirect
 from googletrans import Translator
 from django.conf import settings
+from imp import reload
 
+from .models import mediau
 from .recorder import Recorder
 from watson_developer_cloud import SpeechToTextV1 as SpeechToText
 
@@ -17,6 +23,8 @@ def home(request):
     return render(request, 'base.html')
 
 def convert_file(request):
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
     hide = request.POST['hide']
     print(type(hide))
     if int(hide) == 1:
@@ -25,49 +33,23 @@ def convert_file(request):
         print("Transcribing audio....\n")
         result = transcribe_audio('speech.wav')
     else:
-        result = transcribe_audio_file(settings.MEDIA_ROOT+"\\documents\\audio-file.flac")
-    # file = request.POST.get('abc', False)
-    # uploading the file is required
-    # print("entered2")
-    # form = DocumentForm(request.POST, request.FILES)
-    #
-    # newdoc = Document(docfile = request.FILES.get('file'))
-    # newdoc.save()
-    # if request.method == 'POST' and request.FILES['abc']:
-    #     myfile = request.FILES['abc']
-        # a, file_extension = os.path.splitext(request.FILES['abc'].name)
-        # print("a","ASDADA",file_extension)
-        # cmd="mkdir "+settings.MEDIA_ROOT +'\\'+a
-        # print(cmd)
-        # result = Popen(cmd, shell=True, stdout=PIPE).stdout.read()
-        # if len(result)>0:
-        #     raise Exception(result)
-        #
-        # location=settings.MEDIA_ROOT+"\\documents"
-        # base_url=settings.MEDIA_URL+"/documents"
-        # print(myfile)
-        # fs = FileSystemStorage(location,base_url)
-        # file_name = fs.save(myfile.name, myfile)
-        # uploaded_file_url = fs.url(file_name)
-        # fileurl=mediau(url=uploaded_file_url,name="a")
-        # fileurl.save()
-        # print("success")
+        if request.method == 'POST' and request.FILES['abc']:
+            myfile = request.FILES['abc']
+            filename, file_extension = os.path.splitext(request.FILES['abc'].name)
+            file_extension = file_extension[1:]
+            location = settings.MEDIA_ROOT
+            base_url = settings.MEDIA_URL
+            print(myfile)
+            fs = FileSystemStorage(location, base_url)
+            file_name = fs.save(myfile.name, myfile)
+            uploaded_file_url = fs.url(file_name)
+            fileurl = mediau(url=uploaded_file_url, name=filename)
+            fileurl.save()
 
-    # Load documents for the list page
-    # documents = Document.objects.all()
-    # print("entered4")
-
-    # Render list page with the documents and the form
-    # return render(
-    #     request, 'home.html',
-    #     {'documents': documents, 'form': form},
-    # )
-
-    # after uploading the file
-    # result = transcribe_audio(settings.MEDIA_ROOT+"\\documents\\audio-file.flac")
-    #result = transcribe_audio()
-    print("entered5")
-
+        result = transcribe_audio_file(settings.MEDIA_ROOT+"\\" + filename+"." + file_extension, file_extension)
+        # print(result)
+    # for k, v in result.items():
+    #     print(v[0])
     text = result['results'][0]['alternatives'][0]['transcript']
     print("Text: " + text + "\n")
 
@@ -110,14 +92,13 @@ def transcribe_audio(path_to_audio_file):
         return speech_to_text.recognize(audio_file,content_type='audio/wav')
 
 
-
-def transcribe_audio_file(path_to_audio_file):
+def transcribe_audio_file(path_to_audio_file, file_extension):
     username = "b6edc1f8-d2c3-4aaf-97d3-d16dd0ad1d61"
     password = "Ypj577gNJvPi"
     speech_to_text = SpeechToText(username=username,password=password)
 
     with open(path_to_audio_file, 'rb') as audio_file:
-        return speech_to_text.recognize(audio_file,content_type='audio/flac')
+        return speech_to_text.recognize(audio_file,content_type='audio/'+file_extension)
 
 
 
@@ -150,7 +131,7 @@ def display_results(data):
 def translate_to_tamil(text):
     translator = Translator()
     translated_text = translator.translate(text, dest='ta')
-    print(translated_text)
+    # print(translated_text)
     return translated_text
 
 def translate_to_telugu(text):
